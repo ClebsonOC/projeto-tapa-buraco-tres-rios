679ab94c58ed1ac9b91d0b57aa166c594ab1a811
Clebson Oliveira <oliveiraclebson007@gmail.com>
2025-07-22 13:39:14 -0300

    Mudança nome usuário para bruna.silva

--- src/config.js
+++ src/config.js
@@ -23,7 +23,7 @@ const CACHE_EXPIRATION_BAIRROS_MS = 3600 * 1000 * 24; // 24 horas
 
 // Simulação de Banco de Dados de Usuários
 const USERS_DATABASE = {
-  "mario.silva": { password: "lytoranea", sheetName: "DadosMario" },
+  "bruna.almeida": { password: "lytoranea", sheetName: "DadosBruna" },
   "admin": { password: "lytoranea2025", sheetName: "Dados", role: "admin" }
 };
 
